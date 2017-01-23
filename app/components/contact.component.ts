6d5671710c7ffda0f1ad7808ed400f0db808d373
karajrish <rishabh.karajgi@gmail.com>
2017-01-23 22:39:19 +0530

    first commit

---
+++ app/components/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact',
+  template: `
+  <h1>You can contact me on johndoe@gmail.com</h1>
+  `,
+
+})
+export class ContactComponent  {
+
+ }
